d61e586a483bc5a01bc9a9ebe6e2b52becfcc55b
Yura <kachiko888100@gmail.com>
2019-08-06 00:28:16 +0300

    HM02 done

---
+++ HM02/HM02/HM02/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace HM02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число от 1 до 10");
+            int num01 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число от 10 до 20");
+            int num02 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите знак операции(+ , - , * , / , % , ^ )");
+            var operation = Console.ReadLine();
+
+            switch(operation)
+            {
+                case "+":
+                    Console.WriteLine(num01 + num02);
+                break;
+
+                case "-":
+                    Console.WriteLine(num01 - num02);
+                    break;
+
+                case "*":
+                    Console.WriteLine(num01 * num02);
+                    break;
+
+                case "/":
+                    Console.WriteLine(num01 / num02);
+                    break;
+
+                case "%":
+                    Console.WriteLine(num01 % num02);
+                    break;
+
+                case "^":
+                    Console.WriteLine(Math.Pow(num01, num02));
+                    break;
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
