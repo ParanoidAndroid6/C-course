1a0c03246ad50b0766212130741efbb66b8100fe
Yura <kachiko888100@gmail.com>
2019-08-06 23:19:59 +0300

    HM03 Done

---
+++ HM03/HM03/HM03/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HM03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int result;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = 0; j < numbers.Length; j++)
+                {
+                    result = numbers[i] * numbers[j];
+                    Console.WriteLine($"{numbers[i]} * {numbers[j]} = {result}\t");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
