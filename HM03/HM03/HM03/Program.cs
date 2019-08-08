7edce7b6404226756a454eb368e608f20489cb5c
Yura <kachiko888100@gmail.com>
2019-08-09 01:08:28 +0300

    HM03 Changed

--- HM03/HM03/HM03/Program.cs
+++ HM03/HM03/HM03/Program.cs
@@ -9,16 +9,30 @@ namespace HM03
             int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
             int result;
 
-            for (int i = 0; i < numbers.Length; i++)
+            for (var i = 0; i < numbers.Length; i++)
             {
-                for (int j = 0; j < numbers.Length; j++)
-                {
-                    result = numbers[i] * numbers[j];
-                    Console.WriteLine($"{numbers[i]} * {numbers[j]} = {result}\t");
-                }
+                Console.Write($"  {numbers[i]}\t");
             }
 
+            Console.WriteLine("\n----------------------------------------------------------------------------");
+
+            for (var j = 0; j < numbers.Length; j++)
+            {
+                Console.Write($"  {result = numbers[0] * numbers[j]}\t  " +
+                    $"{result = numbers[1] * numbers[j]}\t  " +
+                    $"{result = numbers[2] * numbers[j]}\t  " +
+                    $"{result = numbers[3] * numbers[j]}\t  " +
+                    $"{result = numbers[4] * numbers[j]}\t  " +
+                    $"{result = numbers[5] * numbers[j]}\t  " +
+                    $"{result = numbers[6] * numbers[j]}\t  " +
+                    $"{result = numbers[7] * numbers[j]}\t  " +
+                    $"{result = numbers[8] * numbers[j]}\t  " +
+                    $"{result = numbers[9] * numbers[j]}\t\n");
+
+
+            }
             Console.ReadKey();
         }
     }
 }
+
