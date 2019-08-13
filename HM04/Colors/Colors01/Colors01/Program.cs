8e2a018e6ffbdff93aa4d3fa551a1ee078a0557b
Yura <kachiko888100@gmail.com>
2019-08-13 11:47:38 +0300

    HM04 is done

---
+++ HM04/Colors/Colors01/Colors01/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Colors01
+{
+    class Program
+    {
+        [Flags]
+        enum Colors: byte
+        {   
+            
+            Black = 1,
+            White = 1<<1,
+            Yellow = 1<<2,
+            Green = 1<<3,
+            Magenta = 1<<4,
+            Grey = 1<<5,
+            Red = 1<<6,
+            Blue = 1<<7,
+        }
+        static void Main(string[] args)
+        {
+            Colors allColors = Colors.Black | Colors.Blue | Colors.Green | Colors.Grey | Colors.Magenta | Colors.Red | Colors.White | Colors.Yellow;
+
+            foreach (var item in Enum.GetValues(typeof(Colors)))
+            {
+                Console.WriteLine($"{item}: {Convert.ToInt32(item)}");
+            }
+
+            Console.WriteLine("Choose four colors to add them in favorites (type their numbers in console): ");
+
+            var input1 = (Colors)Enum.Parse(typeof(Colors), Console.ReadLine());
+            var input2 = (Colors)Enum.Parse(typeof(Colors), Console.ReadLine());
+            var input3 = (Colors)Enum.Parse(typeof(Colors), Console.ReadLine());
+            var input4 = (Colors)Enum.Parse(typeof(Colors), Console.ReadLine());
+
+            Colors favoriteColors = input1 | input2 | input3 | input4;
+
+            Console.WriteLine($"Your favorite colors are: {favoriteColors}");
+
+            Console.WriteLine($"Your unfavorite colors are: {allColors ^ favoriteColors}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HM04/Containers/ContainerHM/ContainerHM/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ContainerHM
+{
+    class Program
+    {
+        [Flags]
+        enum Litres : short
+        {
+            None = 0x0,
+            Litres1 = 0x1,
+            Litres5 = 0x1 << 1,
+            Litres20 = 0x1 << 2
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How much litres of juice do u want?: ");
+            double input = Convert.ToDouble(Console.ReadLine());
+            var non = Litres.None;
+            Litres lit = Litres.Litres1 | Litres.Litres5 | Litres.Litres20;
+            double contain20 = input / 20;
+            double contain20Smth = input % 20;
+            double contain5 = contain20Smth / 5;
+            double contain5Smth = contain20Smth % 5;
+            double contain1 = contain5Smth / 1;
+
+            Console.WriteLine($"20 Litres: {Math.Floor(contain20)}");
+            Console.WriteLine($"5 Litres: {Math.Floor(contain5)}");
+            Console.WriteLine($"1 Litre: {Math.Ceiling(contain1)}");
+
+            Console.ReadKey();
+        }
+    }
+}
