698bdda7012484717851ca14788db98f5b4adfd6
Yura <kachiko888100@gmail.com>
2019-08-22 14:13:31 +0300

    Still doing

---
+++ HM07/CharA/CharAHM/CharAHM/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CharAHM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            while (true)
+            {
+                string input = Console.ReadLine();
+                string[] words = input.Split(" ");
+
+                if (words.Length < 2)
+                {
+                    Console.WriteLine("Введите минимум два слова!");
+                    continue;
+                }
+
+                if (words.Length > 1)
+                {
+                    for (i = 0; i < words.Length; i++)
+                    {
+                        if(words[i].StartsWith('A'))
+                        {
+                            i++;
+                        }
+                    }
+                    break;
+                }
+            }
+            Console.WriteLine($"{i}");
+            Console.ReadKey();
+        }
+    }
+}
